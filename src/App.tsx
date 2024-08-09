989431ca59b411092c4e3f598a6abb636d5d0523
Denis-Kharchuk <denyskharchuk@gmail.com>
2024-08-09 10:14:14 +0300

    Изменил

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
           Link
         </StyledBtn>
         <StyledBtn color="green" fontSize="30px">
-          Hello My good friend
+          Hello My good
         </StyledBtn>
         {/*<input type="text" />
         <input type="submit" />
